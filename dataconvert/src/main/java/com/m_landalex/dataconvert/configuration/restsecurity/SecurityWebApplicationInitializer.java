56a35693f11bcf1ea8437e292dc0c56e9e5fd156
AlMLand <morlandalex@googlemail.com>
2021-03-02 12:12:44 +0100

    Create the security classes for REST.

---
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/configuration/restsecurity/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.m_landalex.dataconvert.configuration.restsecurity;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
