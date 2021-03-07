5d4654e43a62143f2f2f37d9360c43ac495aaff2
AlMLand <morlandalex@googlemail.com>
2021-03-07 01:48:18 +0100

    Create the class rabbit listener.

---
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/service/EmployeeCreateListener.java
@@ -0,0 +1,22 @@
+package com.m_landalex.dataconvert.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeCreateListener {
+
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeCreateListener.class);
+
+	@RabbitListener(containerFactory = "rabbitListenerContainerFactory", queues = "statusCreatingEmployee")
+	public void getCreateStatus(String status) {
+		if (status.equals("succesful")) {
+			logger.info("The object Employee.class are succesful created. [ {} ]", status);
+		} else if (status.equals("error")) {
+			logger.info("The object Employee.class are not crated. [ error ]");
+		}
+	}
+
+}
