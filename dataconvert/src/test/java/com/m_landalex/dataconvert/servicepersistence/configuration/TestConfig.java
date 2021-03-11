5d10ba2a981db5900ebeced0ab5cf98231cb9d1f
AlMLand <morlandalex@googlemail.com>
2021-03-11 18:05:39 +0100

    Create the @Profil("test")

---
+++ dataconvert/src/test/java/com/m_landalex/dataconvert/servicepersistence/configuration/TestConfig.java
@@ -0,0 +1,49 @@
+package com.m_landalex.dataconvert.servicepersistence.configuration;
+
+import javax.sql.DataSource;
+
+import org.dbunit.DataSourceDatabaseTester;
+import org.dbunit.util.fileloader.XlsDataFileLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import com.m_landalex.dataconvert.configuration.DatabaseAccessConfig;
+import com.m_landalex.dataconvert.dbinitialization.DBInitialization;
+
+
+@ComponentScan(basePackages = "com.m_landalex.dataconvert", excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DBInitialization.class) })
+@Profile("test")
+@Configuration
+public class TestConfig {
+
+	private static final Logger logger = LoggerFactory.getLogger(DatabaseAccessConfig.class);
+
+	@Bean
+	public DataSource dataSource() {
+		try {
+			return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+		} catch (Exception e) {
+			logger.error("No datasource created");
+			return null;
+		}
+	}
+
+	@Bean
+	public DataSourceDatabaseTester dataSourceDatabaseTester() {
+		return new DataSourceDatabaseTester(dataSource());
+	}
+
+	@Bean
+	public XlsDataFileLoader xlsDataFileLoader() {
+		return new XlsDataFileLoader();
+	}
+
+}
