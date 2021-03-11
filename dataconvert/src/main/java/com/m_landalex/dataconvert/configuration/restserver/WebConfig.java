950c68bf24318f9eaf62125cf29a6cda1ebb19f9
AlMLand <morlandalex@googlemail.com>
2021-03-11 18:03:13 +0100

    Modified the import

--- dataconvert/src/main/java/com/m_landalex/dataconvert/configuration/restserver/WebConfig.java
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/configuration/restserver/WebConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -16,6 +17,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+@Profile("dev")
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.m_landalex.dataconvert")
