af7413bc9eb51762828641d802b78cc3537d8055
AlMLand <morlandalex@googlemail.com>
2021-03-13 00:34:20 +0100

    Create the init test-db file

---
+++ dataconvert/src/test/resources/db/test-data.sql
@@ -0,0 +1,5 @@
+--insert into user(id, version, username, password, start, userRole, aktiv)
+--values(1, 0, 'test', 'test', 'test', 'test', 'false');
+
+insert into employee(id, version, firstName, lastName, birthDate, jobStartInTheCompany, companyAffiliation, webSite, user_id)
+values(1, 0, 'Test1_First_Name', 'Test1_Last_Name', '1985-06-05', '2018-10-10', 0, 'http://test1.com/', null);
