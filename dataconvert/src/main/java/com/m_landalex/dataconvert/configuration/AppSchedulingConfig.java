8d88b9d037b141bed4ab4b39c62ffced2de74f4f
AlMLand <morlandalex@googlemail.com>
2021-03-13 00:33:44 +0100

    Added to the class the @Profile -> "dev"

--- dataconvert/src/main/java/com/m_landalex/dataconvert/configuration/AppSchedulingConfig.java
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/configuration/AppSchedulingConfig.java
@@ -3,12 +3,14 @@ package com.m_landalex.dataconvert.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Import(AppServiceConfig.class)
 @EnableScheduling
+@Profile("dev")
 @Configuration
 public class AppSchedulingConfig {
 
