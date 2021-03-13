77e80b6620926ab08fb12696711108874dca2d25
AlMLand <morlandalex@googlemail.com>
2021-03-13 14:30:29 +0100

    Added all test methods

--- dataconvert/src/test/java/com/m_landalex/dataconvert/service/EmployeeServiceTest.java
+++ dataconvert/src/test/java/com/m_landalex/dataconvert/service/EmployeeServiceTest.java
@@ -3,13 +3,13 @@ package com.m_landalex.dataconvert.service;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDate;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ActiveProfiles;
@@ -24,14 +24,13 @@ import com.m_landalex.dataconvert.configuration.AppServiceConfig;
 import com.m_landalex.dataconvert.configuration.TransactionManagerConfig;
 import com.m_landalex.dataconvert.data.AbstractObject;
 import com.m_landalex.dataconvert.data.Employee;
+import com.m_landalex.dataconvert.exception.ResourceNullException;
 import com.m_landalex.dataconvert.service.configuration.TestConfig;
 
 @ContextConfiguration(classes = { TestConfig.class, AppServiceConfig.class, TransactionManagerConfig.class } )
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("test")
 public class EmployeeServiceTest {
-	
-	private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceTest.class);
 	
 	@Autowired
 	@Qualifier("employeeService")
@@ -64,6 +63,59 @@ public class EmployeeServiceTest {
 		assertEquals("Test1_Last_Name", returnedEmployee.getLastName());
 		assertEquals(LocalDate.of(1985, 06, 05), returnedEmployee.getBirthDate());
 		assertEquals(LocalDate.of(2018, 10, 10), returnedEmployee.getJobStartInTheCompany());
+	}
+	
+	@SqlGroup( { @Sql( value = "classpath:db/test-data.sql",
+			config = @SqlConfig( encoding = "utf-8", separator = ";", commentPrefix = "--" ),
+			executionPhase = ExecutionPhase.BEFORE_TEST_METHOD ),
+			@Sql( value = "classpath:db/clean-up.sql",
+			config = @SqlConfig( encoding = "utf-8", separator = ";", commentPrefix = "--" ),
+			executionPhase = ExecutionPhase.AFTER_TEST_METHOD ) } )
+	@Test
+	public void saveTest() throws MalformedURLException, ResourceNullException {
+		Employee newEmployee = new Employee();
+		newEmployee.setId(2L);
+		newEmployee.setVersion(0);
+		newEmployee.setFirstName("Test_save_new_employee_firstname");
+		newEmployee.setLastName("Test_save_new_employee_lastname");
+		newEmployee.setBirthDate(LocalDate.of(1987, 02, 02));
+		newEmployee.setJobStartInTheCompany(LocalDate.of(2018, 04, 03));
+		newEmployee.setCompanyAffiliation(0);
+		newEmployee.setWebSite(new URL("http://test_new_employee.com/"));
+		newEmployee.setUser(null);
+		defaultService.save(newEmployee);
+		
+		List<AbstractObject> returnedList = defaultService.fetchAll();
+		assertNotNull(returnedList);
+		assertEquals(2, returnedList.size());
+	}
+	
+	@SqlGroup( { @Sql( value = "classpath:db/test-data.sql",
+			config = @SqlConfig( encoding = "utf-8", separator = ";", commentPrefix = "--" ),
+			executionPhase = ExecutionPhase.BEFORE_TEST_METHOD ),
+			@Sql( value = "classpath:db/clean-up.sql",
+			config = @SqlConfig( encoding = "utf-8", separator = ";", blockCommentStartDelimiter = "--" ),
+			executionPhase = ExecutionPhase.AFTER_TEST_METHOD ) } )
+	@Test
+	public void deleteAllTest() {
+		defaultService.deleteAll();
+		List<AbstractObject> returnedList = defaultService.fetchAll();
+		assertNotNull(returnedList);
+		assertEquals(0, returnedList.size());
+	}
+	
+	@SqlGroup( { @Sql( value = "classpath:db/test-data.sql",
+			config = @SqlConfig( encoding = "utf-8", separator = ";", commentPrefix = "--" ),
+			executionPhase = ExecutionPhase.BEFORE_TEST_METHOD ),
+			@Sql( value = "classpath:db/clean-up.sql",
+			config = @SqlConfig( encoding = "utf-8", separator = ";", commentPrefix = "--" ),
+			executionPhase = ExecutionPhase.AFTER_TEST_METHOD ) } )
+	@Test
+	public void deleteByIdTest() {
+		defaultService.deleteById(1L);
+		List<AbstractObject> returnedList = defaultService.fetchAll();
+		assertNotNull(returnedList);
+		assertEquals(0, returnedList.size());
 	}
 
 }
