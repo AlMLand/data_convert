ac95800795904c837f9c2cae375d0ee8d4c887f2
AlMLand <morlandalex@googlemail.com>
2021-03-14 14:42:00 +0100

    Modified the user data

--- dataconvert/src/test/resources/db/test-data.sql
+++ dataconvert/src/test/resources/db/test-data.sql
@@ -1,5 +1,5 @@
-insert into user(version, username, password, start, userRole, aktiv)
-values(0, 'testUserName', '12345', '2020-12-12', 'DEVELOPER', 'false');
+insert into user(id, version, username, password, start, userRole, aktiv)
+values(2, 0, 'testUserName', '12345', '2020-12-12', 'DEVELOPER', 'false');
 
 insert into employee(id, version, firstName, lastName, birthDate, jobStartInTheCompany, companyAffiliation, webSite, user_id)
 values(1, 0, 'Test1_First_Name', 'Test1_Last_Name', '1985-06-05', '2018-10-10', 0, 'http://test1.com/', null);
