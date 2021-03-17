77b73948b8c14fca69b6738362cb2bed924f7f7a
AlMLand <morlandalex@googlemail.com>
2021-03-17 11:51:22 +0100

    Added the annotation @Profile( "test" )

--- dataconvert/src/test/java/com/m_landalex/dataconvert/service/configuration/TestConfig.java
+++ dataconvert/src/test/java/com/m_landalex/dataconvert/service/configuration/TestConfig.java
@@ -7,28 +7,25 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import com.m_landalex.dataconvert.configuration.DatabaseAccessConfig;
-import com.m_landalex.dataconvert.dbinitialization.DBInitialization;
 
-@ComponentScan( basePackages = "com.m_landalex.dataconvert", excludeFilters = {
-		@ComponentScan.Filter( type = FilterType.ASSIGNABLE_TYPE, value = DBInitialization.class ) } )
-@Profile("test")
+@ComponentScan( basePackages = "com.m_landalex.dataconvert" )
+@Profile( "test" )
 @Configuration
 public class TestConfig {
 
-	private static final Logger logger = LoggerFactory.getLogger(DatabaseAccessConfig.class);
+	private static final Logger logger = LoggerFactory.getLogger( DatabaseAccessConfig.class );
 
 	@Bean
 	public DataSource dataSource() {
 		try {
-			return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
-		} catch (Exception e) {
-			logger.error("No datasource created");
+			return new EmbeddedDatabaseBuilder().setType( EmbeddedDatabaseType.H2 ).build();
+		} catch ( Exception e ) {
+			logger.error( "No datasource created" );
 			return null;
 		}
 	}
