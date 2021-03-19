c69bf0a1735b62c160fe448a4dc6f9eddad24e63
AlMLand <morlandalex@googlemail.com>
2021-03-19 10:02:05 +0100

    Added a filter to the config

--- dataconvert/src/main/java/com/m_landalex/dataconvert/configuration/restserver/WebInitializer.java
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/configuration/restserver/WebInitializer.java
@@ -1,11 +1,15 @@
 package com.m_landalex.dataconvert.configuration.restserver;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.m_landalex.dataconvert.configuration.AppServiceConfig;
 import com.m_landalex.dataconvert.configuration.TransactionManagerConfig;
 import com.m_landalex.dataconvert.configuration.httpremotingserver.HttpInvokerConfigEmployee;
-import com.m_landalex.dataconvert.configuration.restsecurity.SecurityConfig;
+import com.m_landalex.dataconvert.configuration.websecurity.SecurityConfig;
 
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -22,6 +26,12 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter("utf-8", true);
+		return new Filter[] { new HiddenHttpMethodFilter(), encodingFilter };
 	}
 
 }
