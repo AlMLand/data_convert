b8d93edcb63456db29c4fb765c5be5b34e34f071
AlMLand <morlandalex@googlemail.com>
2021-03-19 11:29:55 +0100

    Rename to -> Rest... ; move to package controller.rest; removed the logger

--- dataconvert/src/main/java/com/m_landalex/dataconvert/view/controller/rest/RestUserController.java
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/view/controller/rest/RestUserController.java
@@ -1,9 +1,7 @@
-package com.m_landalex.dataconvert.view.controller;
+package com.m_landalex.dataconvert.view.controller.rest;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -20,10 +18,8 @@ import com.m_landalex.dataconvert.service.DefaultService;
 
 @Controller
 @RequestMapping(value = "/rest/employees/users")
-public class UserController {
+public class RestUserController {
 
-	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-	
 	@Autowired
 	@Qualifier(value = "userService")
 	private DefaultService defaultService;
@@ -57,14 +53,12 @@ public class UserController {
 	@ResponseBody
 	public void updateUserById(@RequestBody User user) throws ResourceNullException {
 		defaultService.save(user);
-		logger.info("User with id {} is updated.", user.getId());
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	@ResponseBody
 	public void deleteUserById(@PathVariable Long id) {
 		defaultService.deleteById(id);
-		logger.info("User with id {} is deleted.", id);
 	}
 	
 }
