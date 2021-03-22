5260e3ff3c5d36dd10ec2bd1313db62061275569
AlMLand <morlandalex@googlemail.com>
2021-03-22 13:11:30 +0100

    Create a new controller for the class Employee

---
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/view/controller/web/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.m_landalex.dataconvert.view.controller.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.m_landalex.dataconvert.data.Employee;
+import com.m_landalex.dataconvert.service.DefaultService;
+
+@Controller
+@RequestMapping( value = "/employees" )
+public class EmployeeController {
+
+	@Autowired
+	@Qualifier( value = "employeeService" )
+	private DefaultService defaultService;
+	private MessageSource messageSource;
+	
+	@RequestMapping( method = RequestMethod.GET )
+	public String list( Model model ) {
+		List<Employee> returnedList = defaultService.fetchAll();
+		model.addAttribute("employees", returnedList);
+		return "employees/list";
+	}
+	
+}
