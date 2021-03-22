8dbed78d09c40d71d357cefd6b965fa8b84d510c
AlMLand <morlandalex@googlemail.com>
2021-03-22 15:45:26 +0100

    Create a class UserController.class

---
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/view/controller/web/UserController.java
@@ -0,0 +1,30 @@
+package com.m_landalex.dataconvert.view.controller.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.m_landalex.dataconvert.data.User;
+import com.m_landalex.dataconvert.service.DefaultService;
+
+@Controller
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@Autowired
+	@Qualifier(value = "userService")
+	private DefaultService defaultService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String list(Model model) {
+		List<User> returnedList = defaultService.fetchAll();
+		model.addAttribute("users", returnedList);
+		return "users/list";
+	}
+	
+}
