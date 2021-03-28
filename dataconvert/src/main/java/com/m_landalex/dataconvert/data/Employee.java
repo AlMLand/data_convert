90d2e1b83e3cf44c851ef5979bed0cc3c76ceafb
AlMLand <morlandalex@googlemail.com>
2021-03-28 15:36:54 +0200

    Implemented the Comparable interface with final Comaprator

--- dataconvert/src/main/java/com/m_landalex/dataconvert/data/Employee.java
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/data/Employee.java
@@ -3,6 +3,8 @@ package com.m_landalex.dataconvert.data;
 import java.io.Serializable;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.Comparator;
+import static java.util.Comparator.*;
 
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.Min;
@@ -25,7 +27,12 @@ import lombok.ToString;
 @SuppressWarnings("serial")
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class Employee extends AbstractObject implements Serializable{
+public class Employee extends AbstractObject implements Serializable, Comparable<Employee> {
+	
+	private static final Comparator<Employee> COMPARATOR = comparing(Employee::getBirthDate)
+			.thenComparing(Employee::getLastName, String.CASE_INSENSITIVE_ORDER)
+			.thenComparing(Employee::getFirstName, String.CASE_INSENSITIVE_ORDER)
+			.thenComparingInt((Employee e) -> e.getCompanyAffiliation());
 	
 	@NotEmpty( message = "{javax.validation.constraints.NotEmpty.message}" )
 	@Size( min = 2, max = 50, message = "{javax.validation.constraints.Size.message}" )
@@ -46,5 +53,10 @@ public class Employee extends AbstractObject implements Serializable{
 	private URL webSite;
 	@NotNull( message = "{javax.validation.constraints.NotNull.message}" )
 	private User user;
+	
+	@Override
+	public int compareTo(Employee o) {
+		return COMPARATOR.compare(this, o);
+	}
 
 }
--- dataconvert/src/main/java/com/m_landalex/dataconvert/data/User.java
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/data/User.java
@@ -2,6 +2,8 @@ package com.m_landalex.dataconvert.data;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Comparator;
+import static java.util.Comparator.*;
 
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotEmpty;
@@ -13,15 +15,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @SuppressWarnings("serial")
+@ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class User extends AbstractObject implements Serializable{
+public class User extends AbstractObject implements Serializable, Comparable<User> {
 
+	private static final Comparator<User> COMPARATOR = comparing(User::getUserRole)
+			.thenComparing(User::getUsername, String.CASE_INSENSITIVE_ORDER);
+	
 	@NotEmpty( message = "{javax.validation.constraints.NotEmpty.message}" ) 
 	@Size(min = 2, max = 50, message = "{javax.validation.constraints.Size.message}" ) 
 	private String username;
@@ -34,5 +41,10 @@ public class User extends AbstractObject implements Serializable{
 	private Boolean aktiv;
 	@NotNull( message = "{javax.validation.constraints.NotNull.message}" )
 	private Role userRole;
+	
+	@Override
+	public int compareTo(User o) {
+		return COMPARATOR.compare(this, o);
+	}
 	
 }
