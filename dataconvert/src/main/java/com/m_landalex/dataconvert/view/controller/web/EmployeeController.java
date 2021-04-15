b61d869c285ac4b540b1de2f59db5ec27148671d
amo <amo@DESKTOP-NI9PUTO.fritz.box>
2021-04-15 18:10:34 +0200

    Added new methods for object update(updateForm(), update())

--- dataconvert/src/main/java/com/m_landalex/dataconvert/view/controller/web/EmployeeController.java
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/view/controller/web/EmployeeController.java
@@ -2,29 +2,32 @@ package com.m_landalex.dataconvert.view.controller.web;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.m_landalex.dataconvert.data.Employee;
+import com.m_landalex.dataconvert.exception.ResourceNullException;
 import com.m_landalex.dataconvert.service.DefaultService;
 
 @Controller
-@RequestMapping( value = "/employees" )
+@RequestMapping(value = "/employees")
 public class EmployeeController {
 
 	@Autowired
-	@Qualifier( value = "employeeService" )
+	@Qualifier(value = "employeeService")
 	private DefaultService defaultService;
-	private MessageSource messageSource;
 	
-	@RequestMapping( method = RequestMethod.GET )
-	public String list( Model model ) {
+	@RequestMapping(method = RequestMethod.GET)
+	public String list(Model model) {
 		List<Employee> returnedList = defaultService.fetchAll();
 		model.addAttribute("employees", returnedList);
 		return "employees/list";
@@ -35,6 +38,23 @@ public class EmployeeController {
 		Employee returnedEmployee = (Employee) defaultService.fetchById(id);
 		model.addAttribute("returnedEmployee", returnedEmployee);
 		return "employees/show";
+	}
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String updateForm(@PathVariable Long id, Model model) {
+		model.addAttribute("employeeToUpdate", defaultService.fetchById(id));
+		return "employees/update";
+	}
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
+	public String update(@Valid @ModelAttribute("employeeToUpdate")Employee employee, BindingResult bindingResult, 
+			Model model) throws ResourceNullException {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("employeeToUpdate", employee);
+			return "employees/update";
+		}
+		defaultService.save(employee);
+		return "redirect:/employees/" + employee.getId();
 	}
 	
 }
