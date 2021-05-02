fdcb6cfea1b6bea1d2d8db13da8c48e8970fe848
AlMLand <morlandalex@googlemail.com>
2021-05-02 22:01:22 +0200

    Allow all home page, modified the method configure(HttpSecurity http)

--- dataconvert/src/main/java/com/m_landalex/dataconvert/configuration/websecurity/SecurityConfig.java
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/configuration/websecurity/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,7 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig {
 
 	@Order(1)
-	@Configuration
+	@EnableGlobalMethodSecurity(prePostEnabled = true)
 	public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		@Autowired
@@ -32,6 +33,7 @@ public class SecurityConfig {
 		protected void configure(HttpSecurity http) throws Exception {
 			http.authorizeRequests()
 			.antMatchers("/employees/**", "/users/**").authenticated()
+			.antMatchers("/**").permitAll()
 			.and()
 			.formLogin()
 			.and()
