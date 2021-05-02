9105e68868d2ed839ac9c1d410a0b8d47238e151
AlMLand <morlandalex@googlemail.com>
2021-05-02 02:27:28 +0200

    Removed the old enum class and created a new class Role

--- dataconvert/src/main/java/com/m_landalex/dataconvert/data/Role.java
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/data/Role.java
@@ -1,7 +1,37 @@
 package com.m_landalex.dataconvert.data;
 
-public enum Role {
+import java.io.Serializable;
+import java.util.Comparator;
 
-	SUPPORTER, ADMINISTRATOR, DEVELOPER;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@SuppressWarnings("serial")
+@EqualsAndHashCode(callSuper = true)
+public class Role extends AbstractObject implements Serializable, Comparable<Role>{
+
+	private static final Comparator<Role> COMPARATOR = Comparator.comparing(Role::getRole, String.CASE_INSENSITIVE_ORDER);
+	
+	@NotBlank(message = "{javax.validation.constraints.NotBlank.message}")
+	private String role;
+
+	@Override
+	public int compareTo(Role o) {
+		return COMPARATOR.compare(this, o);
+	}
+
+	@Override
+	public String toString() {
+		return role;
+	}
 	
 }
