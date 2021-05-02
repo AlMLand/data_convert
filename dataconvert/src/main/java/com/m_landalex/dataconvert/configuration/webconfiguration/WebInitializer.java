d64a1b2d7c7675050a9adf045f84bf43d22f7fe5
AlMLand <morlandalex@googlemail.com>
2021-05-02 02:44:23 +0200

    Added the new SecurityConfig.class to the WebInitializer

--- dataconvert/src/main/java/com/m_landalex/dataconvert/configuration/webconfiguration/WebInitializer.java
+++ dataconvert/src/main/java/com/m_landalex/dataconvert/configuration/webconfiguration/WebInitializer.java
@@ -12,13 +12,14 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 import com.m_landalex.dataconvert.configuration.AppServiceConfig;
 import com.m_landalex.dataconvert.configuration.TransactionManagerConfig;
 import com.m_landalex.dataconvert.configuration.httpremotingserver.HttpInvokerConfigEmployee;
-import com.m_landalex.dataconvert.configuration.websecurity.RestSecurityConfig;
+import com.m_landalex.dataconvert.configuration.websecurity.SecurityConfig;
 
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class<?>[] { AppServiceConfig.class, TransactionManagerConfig.class, RestSecurityConfig.class };
+		return new Class<?>[] { AppServiceConfig.class, TransactionManagerConfig.class, 
+			SecurityConfig.RestSecurityConfig.class, SecurityConfig.WebSecurityConfig.class };
 	}
 
 	@Override
